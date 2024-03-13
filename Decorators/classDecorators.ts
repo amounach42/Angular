7f5f216eaf47834f0662cae84ad6f777c318908b
Abdessalam Mounach <amounach@smb.sqli.com>
2024-03-13 09:07:34 +0100

    chore: implement class decorator logged

--- Decorators/classDecorators.ts
+++ Decorators/classDecorators.ts
@@ -4,5 +4,18 @@
     new instance of a class is created.
 */
 
-function logged(constructor: Function)
-{}
+function Logged(constructor: Function, ...args: any[]): void
+{
+    console.log("logged decorator called!")
+    console.log(constructor)
+}
+
+@Logged
+class User {
+   private name: string;
+
+   constructor(private _name: string) {
+    console.log("User constructor called!")
+    this._name = this.name;
+   }
+}
